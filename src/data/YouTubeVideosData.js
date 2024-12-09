ef7d241b98f9fe20bbf08720bce1310825b073fc
Francisco Perlaza <franrodriguezc130@gmail.com>
2024-12-09 16:49:49 -0500

    New YouTube video to the video section on the web

--- src/data/YouTubeVideosData.js
+++ src/data/YouTubeVideosData.js
@@ -4,8 +4,16 @@ const youtubeVideos = [
     id: '1',
     title: 'Una mirada al 2045 | Inteligencia Artificial',
     description: 'Exploro cómo las últimas tecnologías de inteligencia artificial están transformando el mundo, mostrando aplicaciones prácticas y su potencial para resolver problemas en diversos campos. Acompáñame para entender y aprovechar estas herramientas en tus proyectos.',
-    thumbnail: 'https://img.youtube.com/vi/VIDEO_ID_1/hqdefault.jpg', // Reemplaza VIDEO_ID_1 por el ID real del video
+    thumbnail: 'https://img.youtube.com/vi/MzQeGN0qY5Q/hqdefault.jpg', // Reemplazar VIDEO_ID_1 por el ID real del video
     videoUrl: 'https://www.youtube.com/embed/MzQeGN0qY5Q',
+  },
+
+  {
+    id: '2',
+    title: 'Aprender a Programar Desde Cero | Programación',
+    description: 'Exploro desde los conceptos básicos de programación hasta temas avanzados, ofreciendo tutoriales, guías y proyectos prácticos. Ya sea que estés iniciando en el mundo del desarrollo o buscando perfeccionar tus habilidades, aquí encontrarás recursos y conocimientos útiles para llevar tus proyectos al siguiente nivel.',
+    thumbnail: 'https://img.youtube.com/vi/SRNAJ8Kf-ec/hqdefault.jpg', // Reemplazar VIDEO_ID_1 por el ID real del video
+    videoUrl: 'https://www.youtube.com/embed/SRNAJ8Kf-ec',
   },
 ];
 
